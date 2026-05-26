af580d66371a165edf1dfd8ae9d8b18157a6bb58
Emersonripardo <113382003+Emersonripardo@users.noreply.github.com>
2026-05-26 18:12:06 -0400

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -23,6 +23,36 @@ app.get("/", (req, res) => {
 
 })
 
+// ======================================
+// CURSOS CEFIS
+// ======================================
+
+app.get("/courses", async (req,res)=>{
+
+  try{
+
+    const response =
+    await fetch(
+      "https://api-v3.cefis.com.br/courses"
+    )
+
+    const data =
+    await response.json()
+
+    res.json(data)
+
+  }catch(error){
+
+    console.error(error)
+
+    res.status(500).json({
+      error:"Erro cursos"
+    })
+
+  }
+
+})
+
 const PORT = process.env.PORT || 3000
 
 // CHAT OPENAI
